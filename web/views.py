338824644a5902385016ca29473bf5597845abbd
Hamid <hamid.roohany@gmail.com>
2022-05-11 14:02:53 +0430

    .

--- web/views.py
+++ web/views.py
@@ -1,7 +1,13 @@
+from json import JSONEncoder
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 # Create your views here.
 
 def submit_expense(request):
-    print("we are here")
+    print(request.POST)
+
+    return JsonResponse({
+        'status': 'true',
+    }, encoder=JSONEncoder)
